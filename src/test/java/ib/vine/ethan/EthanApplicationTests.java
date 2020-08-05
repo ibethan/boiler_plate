cc4c9cf1d7b91e4caa42cd22752060db7b5f2eac
Sanghui Park <uhaha@ncsoft.com>
2020-08-05 13:47:55 +0900

    Initial commit

---
+++ src/test/java/ib/vine/ethan/EthanApplicationTests.java
@@ -0,0 +1,13 @@
+package ib.vine.ethan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
